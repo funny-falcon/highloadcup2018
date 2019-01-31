87da6831c95e72d90807142c5aec03881a991528
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2019-01-31 07:34:06 +0300

    a bit faster

--- bitmap3/bitmap.go
+++ bitmap3/bitmap.go
@@ -2,7 +2,6 @@ package bitmap3
 
 type Bitmap struct {
 	Size uint32
-	L1   [48]uint32
 	L2   [1536]uint32
 	L3   [49152]uint32
 }
@@ -11,7 +10,6 @@ func (bm *Bitmap) Set(ix int32) {
 	if Set(bm.L3[:], ix) {
 		bm.Size++
 		Set(bm.L2[:], ix/32)
-		Set(bm.L1[:], ix/(32*32))
 	}
 }
 
@@ -22,39 +20,28 @@ func (bm *Bitmap) Unset(ix int32) {
 }
 
 func (bm *Bitmap) Has(ix int32) bool {
-	return Has(bm.L1[:], ix/(32*32)) && Has(bm.L2[:], ix/32) && Has(bm.L3[:], ix)
+	return Has(bm.L2[:], ix/32) && Has(bm.L3[:], ix)
 }
 
 func (bm *Bitmap) LoopBlock(f func(int32, uint32) bool) {
-	var l1u, l2u Unrolled
-	for l1ix := int32(len(bm.L1) - 1); l1ix >= 0; l1ix-- {
-		l1v := bm.L1[l1ix]
-		if l1v == 0 {
+	var l2u Unrolled
+	for l2ix := int32(len(bm.L2) - 1); l2ix >= 0; l2ix-- {
+		l2v := bm.L2[l2ix]
+		if l2v == 0 {
 			continue
 		}
-		l1ixb := l1ix * 32
-		for _, l2ix := range Unroll(l1v, l1ixb, &l1u) {
-			l2v := bm.L2[l2ix]
-			l2ixb := l2ix * 32
-			for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
-				l3v := bm.L3[l3ix]
-				if l3v != 0 && !f(l3ix*32, l3v) {
-					return
-				}
+		l2ixb := l2ix * 32
+		for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
+			l3v := bm.L3[l3ix]
+			if l3v != 0 && !f(l3ix*32, l3v) {
+				return
 			}
 		}
 	}
 }
 
-func (bm *Bitmap) GetL1() *[48]uint32 {
-	return &bm.L1
-}
-
-func (bm *Bitmap) GetL2(l2ix int32) uint32 {
-	if !Has(bm.L1[:], l2ix) {
-		return 0
-	}
-	return bm.L2[l2ix]
+func (bm *Bitmap) GetL2() *[1536]uint32 {
+	return &bm.L2
 }
 
 func (bm *Bitmap) GetBlock(span int32) uint32 {
--- bitmap3/iterator.go
+++ bitmap3/iterator.go
@@ -4,17 +4,16 @@ import "sort"
 
 type IBitmap interface {
 	LoopBlock(func(int32, uint32) bool)
-	GetL1() *[48]uint32
-	GetL2(int32) uint32
+	GetL2() *[1536]uint32
 	GetBlock(int32) uint32
 	Has(int32) bool
 }
 
 type NullBitmap struct{}
 
+func (NullBitmap) Loop(f func([]int32) bool)          {}
 func (NullBitmap) LoopBlock(func(int32, uint32) bool) {}
-func (NullBitmap) GetL1() *[48]uint32                 { return &nullL1 }
-func (NullBitmap) GetL2(int32) uint32                 { return 0 }
+func (NullBitmap) GetL2() *[1536]uint32               { panic("no"); return nil }
 func (NullBitmap) GetBlock(int32) uint32              { return 0 }
 func (NullBitmap) Has(int32) bool                     { return false }
 
@@ -25,8 +24,7 @@ type RawUids []int32
 func (r RawUids) Loop(f func([]int32) bool)        { f(r) }
 func (r RawUids) Count() uint32                    { return uint32(len(r)) }
 func (RawUids) LoopBlock(func(int32, uint32) bool) { panic("no") }
-func (RawUids) GetL1() *[48]uint32                 { panic("no"); return &nullL1 }
-func (RawUids) GetL2(int32) uint32                 { panic("no"); return 0 }
+func (RawUids) GetL2() *[1536]uint32               { panic("no"); return nil }
 func (RawUids) GetBlock(int32) uint32              { panic("no"); return 0 }
 func (RawUids) Has(int32) bool                     { panic("no"); return false }
 
@@ -36,8 +34,7 @@ type RawWithMap struct {
 }
 
 func (RawWithMap) LoopBlock(func(int32, uint32) bool) { panic("no") }
-func (RawWithMap) GetL1() *[48]uint32                 { panic("no"); return &nullL1 }
-func (RawWithMap) GetL2(int32) uint32                 { panic("no"); return 0 }
+func (RawWithMap) GetL2() *[1536]uint32               { panic("no"); return nil }
 func (RawWithMap) GetBlock(int32) uint32              { panic("no"); return 0 }
 func (RawWithMap) Has(int32) bool                     { panic("no"); return false }
 
@@ -54,10 +51,8 @@ func (r RawWithMap) Loop(f func([]int32) bool) {
 }
 
 type AndBitmap struct {
-	Maps     []IBitmap
-	L1       [48]uint32
-	L2Filled [48]uint32
-	L2       [1536]uint32
+	Maps []IBitmap
+	L2   [1536]uint32
 
 	LastSpan  int32
 	LastBlock uint32
@@ -84,82 +79,57 @@ func NewAndBitmap(maps []IBitmap) IBitmap {
 				R: raw,
 				M: NewAndBitmap(append(maps[:i], maps[i+1:]...)),
 			}
+		} else if _, ok := m.(NullBitmap); ok {
+			return m
 		}
 	}
 	sort.Slice(maps, func(i, j int) bool {
 		return cnt(maps[i]) < cnt(maps[j])
 	})
 	bm := &AndBitmap{Maps: maps}
-	for i := range bm.L1 {
-		bm.L1[i] = ^uint32(0)
+	for i := range bm.L2 {
+		bm.L2[i] = ^uint32(0)
 	}
 	for _, m := range maps {
-		ml1 := m.GetL1()
-		for i := range bm.L1 {
-			bm.L1[i] &= ml1[i]
+		for i, v := range m.GetL2() {
+			bm.L2[i] &= v
 		}
 	}
 	return bm
 }
 
 func (bm *AndBitmap) LoopBlock(f func(int32, uint32) bool) {
-	var l1u, l2u Unrolled
-	for l1ix := int32(len(bm.L1) - 1); l1ix >= 0; l1ix-- {
-		l1v := bm.L1[l1ix]
-		if l1v == 0 {
+	var l2u Unrolled
+	for l2ix := int32(len(bm.L2) - 1); l2ix >= 0; l2ix-- {
+		l2v := bm.L2[l2ix]
+		if l2v == 0 {
 			continue
 		}
-		l1ixb := l1ix * 32
-		for _, l2ix := range Unroll(l1v, l1ixb, &l1u) {
-			if !Has(bm.L2Filled[:], l2ix) {
-				bm.FillL2(l2ix)
-			}
-			l2v := bm.L2[l2ix]
-			if l2v == 0 {
-				continue
-			}
-			l2ixb := l2ix * 32
-		l3loop:
-			for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
-				l3v := ^uint32(0)
-				l3ixb := l3ix * 32
-				for _, m := range bm.Maps {
-					l3v &= m.GetBlock(l3ixb)
-					if l3v == 0 {
-						continue l3loop
-					}
+		l2ixb := l2ix * 32
+	l3loop:
+		for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
+			l3v := ^uint32(0)
+			l3ixb := l3ix * 32
+			for _, m := range bm.Maps {
+				l3v &= m.GetBlock(l3ixb)
+				if l3v == 0 {
+					continue l3loop
 				}
-				if l3v != 0 && !f(l3ixb, l3v) {
-					return
-				}
+			}
+			if l3v != 0 && !f(l3ixb, l3v) {
+				return
 			}
 		}
 	}
 }
 
-func (bm *AndBitmap) GetL1() *[48]uint32 {
-	return &bm.L1
-}
-
-func (bm *AndBitmap) GetL2(l2ix int32) uint32 {
-	if !Has(bm.L1[:], l2ix) {
-		return 0
-	}
-	if !Has(bm.L2Filled[:], l2ix) {
-		bm.FillL2(l2ix)
-	}
-	return bm.L2[l2ix]
+func (bm *AndBitmap) GetL2() *[1536]uint32 {
+	return &bm.L2
 }
 
 func (bm *AndBitmap) GetBlock(span int32) uint32 {
 	if span == bm.LastSpan {
 		return bm.LastBlock
-	}
-	if !Has(bm.L1[:], span/(32*32)) {
-		return 0
-	}
-	if !Has(bm.L2Filled[:], span/(32*32)) {
-		bm.FillL2(span / (32 * 32))
 	}
 	if !Has(bm.L2[:], span/32) {
 		return 0
@@ -183,23 +153,9 @@ func (bm *AndBitmap) Has(ix int32) bool {
 	return bl&b != 0
 }
 
-func (bm *AndBitmap) FillL2(l2ix int32) {
-	l2v := ^uint32(0)
-	for _, m := range bm.Maps {
-		l2v &= m.GetL2(l2ix)
-	}
-	bm.L2[l2ix] = l2v
-	if l2v == 0 {
-		Unset(bm.L1[:], l2ix)
-	}
-	Set(bm.L2Filled[:], l2ix)
-}
-
 type OrBitmap struct {
-	Maps     []IBitmap
-	L1       [48]uint32
-	L2Filled [48]uint32
-	L2       [1536]uint32
+	Maps []IBitmap
+	L2   [1536]uint32
 
 	LastSpan  int32
 	LastBlock uint32
@@ -213,69 +169,42 @@ func NewOrBitmap(maps []IBitmap) IBitmap {
 		return maps[0]
 	}
 	bm := &OrBitmap{Maps: maps}
-	for i := range bm.L1 {
-		bm.L1[i] = 0
-	}
 	for _, m := range maps {
-		ml1 := m.GetL1()
-		for i := range bm.L1 {
-			bm.L1[i] |= ml1[i]
+		for i, v := range m.GetL2() {
+			bm.L2[i] |= v
 		}
 	}
 	return bm
 }
 
 func (bm *OrBitmap) LoopBlock(f func(int32, uint32) bool) {
-	var l1u, l2u Unrolled
-	for l1ix := int32(len(bm.L1) - 1); l1ix >= 0; l1ix-- {
-		l1v := bm.L1[l1ix]
-		if l1v == 0 {
+	var l2u Unrolled
+	for l2ix := int32(len(bm.L2) - 1); l2ix >= 0; l2ix-- {
+		l2v := bm.L2[l2ix]
+		if l2v == 0 {
 			continue
 		}
-		l1ixb := l1ix * 32
-		for _, l2ix := range Unroll(l1v, l1ixb, &l1u) {
-			if !Has(bm.L2Filled[:], l2ix) {
-				bm.FillL2(l2ix)
+		l2ixb := l2ix * 32
+		for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
+			l3v := uint32(0)
+			l3ixb := l3ix * 32
+			for _, m := range bm.Maps {
+				l3v |= m.GetBlock(l3ixb)
 			}
-			l2v := bm.L2[l2ix]
-			l2ixb := l2ix * 32
-			for _, l3ix := range Unroll(l2v, l2ixb, &l2u) {
-				l3v := uint32(0)
-				l3ixb := l3ix * 32
-				for _, m := range bm.Maps {
-					l3v |= m.GetBlock(l3ixb)
-				}
-				if l3v != 0 && !f(l3ixb, l3v) {
-					return
-				}
+			if l3v != 0 && !f(l3ixb, l3v) {
+				return
 			}
 		}
 	}
 }
 
-func (bm *OrBitmap) GetL1() *[48]uint32 {
-	return &bm.L1
-}
-
-func (bm *OrBitmap) GetL2(l2ix int32) uint32 {
-	if !Has(bm.L1[:], l2ix) {
-		return 0
-	}
-	if !Has(bm.L2Filled[:], l2ix) {
-		bm.FillL2(l2ix)
-	}
-	return bm.L2[l2ix]
+func (bm *OrBitmap) GetL2() *[1536]uint32 {
+	return &bm.L2
 }
 
 func (bm *OrBitmap) GetBlock(span int32) uint32 {
 	if span == bm.LastSpan {
 		return bm.LastBlock
-	}
-	if !Has(bm.L1[:], span/(32*32)) {
-		return 0
-	}
-	if !Has(bm.L2Filled[:], span/(32*32)) {
-		bm.FillL2(span / (32 * 32))
 	}
 	if !Has(bm.L2[:], span/32) {
 		return 0
@@ -293,13 +222,4 @@ func (bm *OrBitmap) Has(ix int32) bool {
 	bl := bm.GetBlock(ix &^ 31)
 	b := uint32(1) << uint32(ix&31)
 	return bl&b != 0
-}
-
-func (bm *OrBitmap) FillL2(l2ix int32) {
-	l2v := uint32(0)
-	for _, m := range bm.Maps {
-		l2v |= m.GetL2(l2ix)
-	}
-	bm.L2[l2ix] = l2v
-	Set(bm.L2Filled[:], l2ix)
 }
